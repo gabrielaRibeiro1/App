0b2c1cf9f813ec9a05e31adcbdbda115faae2ef0
gabrielar <gabrielatr200@gmail.com>
2023-03-07 14:53:13 +0000

    semana 3 - adicionar funções aos botões

--- app/src/main/java/ipvc/estg/app/MainActivity.kt
+++ app/src/main/java/ipvc/estg/app/MainActivity.kt
@@ -3,37 +3,45 @@ package ipvc.estg.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MY_TAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"onCreate")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("****TAG","onPause")
+        Log.d(TAG,"onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("****TAG","onStop")
+        Log.d(TAG,"onStop")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("****TAG","onStart")
+        Log.d(TAG,"onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("****TAG","onResume")
+        Log.d(TAG,"onResume")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("****TAG","onDestroy")
+        Log.d(TAG,"onDestroy")
+    }
 
+    fun ButtonClick(view: View) {
+        Toast.makeText(this,R.string.KEY_WELCOME, Toast.LENGTH_LONG).show()
     }
 
 
