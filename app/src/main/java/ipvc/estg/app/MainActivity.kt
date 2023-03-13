0e569c9ebd08201a6e6fbcb34768d3952741276b
gabrielar <gabrielatr200@gmail.com>
2023-03-13 16:23:52 +0000

    semana 3 - adicionar funções aos botões

--- app/src/main/java/ipvc/estg/app/MainActivity.kt
+++ app/src/main/java/ipvc/estg/app/MainActivity.kt
@@ -1,9 +1,12 @@
 package ipvc.estg.app
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG,"onCreate")
+    }
+
+
+
+    fun onAction(view: View) {
+        val t = findViewById<TextView>(R.id.b1)
+        val e1 = findViewById<EditText>(R.id.b2)
+        val e2 = findViewById<EditText>(R.id.b3)
+        val e3 = findViewById<EditText>(R.id.b4)
+
+        val x= ( e1.text.toString().toInt() + e2.text.toString().toInt()+ e3.text.toString().toInt())
+
+        t.setText("Resultado: $x")
+
+
     }
 
     override fun onPause() {
